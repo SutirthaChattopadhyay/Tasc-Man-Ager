be167b9bb1f56ea10c0496f67116b4acc9457b47
Sutirtha <forncvlogss@gmail.com>
2026-06-17 10:39:15 +0530

    implemented add and view func, redefined UI

--- main.py
+++ main.py
@@ -14,6 +14,23 @@ class TaskManager:
         print("4. Delete Task")
         print("5. Exit")    
 
+    def add_task(self):
+        task = input("Enter Task: ")
+        self.tasks.append(task)
+
+        print("Task Added Successfully")    
+
+
+    def view_tasks(self):
+        if(self.tasks) == 0:
+            print("I Can't see anything ")
+            return
+        
+        print("\nTasks:")
+
+        for i, task in enumerate(self.tasks, start=1):
+            print(f"{i}.{task}")
+
 manager = TaskManager()
 
 while True:
@@ -24,12 +41,18 @@ while True:
     
     if choice == "1":
         print("View Tasks Selected")
+        print("------------------------")
+        manager.view_tasks()
     elif choice == "2":
         print("Add Tasks Selected")
+        print("------------------------")
+        manager.add_task()
     elif choice == "3":
         print("Complete Tasks Selected")
+        print("------------------------")
     elif choice == "4":
         print("Delete Tasks Selected")
+        print("------------------------")
     elif choice == "5":
         print("GoodByee!")
         break
